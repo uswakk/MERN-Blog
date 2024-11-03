4afcfee1441cf0a2303a02998f2db753ba3ca943
Uswa Khan <101996375+uswakk@users.noreply.github.com>
2024-11-03 20:38:28 +0500

    Update api.js
    changes the url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:3000"
+const URL = "https://mern-blog-5zdq.onrender.com"
 export default async function getPosts(){
     const response = await axios.get(`${URL}/posts`)
 
