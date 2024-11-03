8f8db09eb57b91795ec1b5b7809a4f9f3046e942
Uswa Khan <101996375+uswakk@users.noreply.github.com>
2024-11-03 19:54:56 +0500

    Update server.js
    Chnaged the port

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const posts = require("./postRoutes");
 const users = require("./userRoutes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
 
 app.use(cors());
 app.use(express.json());
